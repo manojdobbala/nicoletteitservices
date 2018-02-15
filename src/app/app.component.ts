6a2784853e42dbaf2b115d42732b6775f412f16b
Manoj Dobbala <manoj_k_dobbala@homedepot.com>
2018-02-14 21:31:05 -0600

    Intial commit for Nicolette It services

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+declare var $: any;
+declare var jQuery: any;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor() {
+  }
+}
